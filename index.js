77b835a6cda6ff9d8619a34846a09dfd45c0111c
Krishna Agrawal <120992509+krishnaagrawal7508@users.noreply.github.com>
2023-10-11 16:25:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// image 1.
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage1);
+
+
+// image 2.
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+
+// headings.
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").textContent = "🚩Player 1 Wins!";
+}
+
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins🚩";
+}
+
+else {
+    document.querySelector("h1").innerHTML="Draw";
+}
